5dbd3edf70ae464d9aa89db0a1c5e6cd1a0a0e1b
AlphaStalker <m.szczepanski128@gmail.com>
2023-12-17 16:14:54 +0100

    Break part 1 solution into functions

--- day_01/part1.py
+++ day_01/part1.py
@@ -1,17 +1,24 @@
+def get_number_out_of_line(line: str) -> int:
+    result = 0
+
+    for char in line:
+        if char.isdigit():
+            result += 10 * int(char)
+            break
+
+    for char in line[::-1]:
+        if char.isdigit():
+            result += int(char)
+            break
+
+    return result
+
+
 def solution(text_lines: list[str]) -> int:
     result = 0
 
     for line in text_lines:
-
-        for char in line:
-            if char.isdigit():
-                result += 10 * int(char)
-                break
-        
-        for char in line[::-1]:
-            if char.isdigit():
-                result += int(char)
-                break
+        result += get_number_out_of_line(line)
 
     return result
 
