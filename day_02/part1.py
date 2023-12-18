9430a03262e19d29136c6e6523bf961dbe23674b
AlphaStalker <m.szczepanski128@gmail.com>
2023-12-18 18:27:58 +0100

    Refactor part 1

--- day_02/part1.py
+++ day_02/part1.py
@@ -1,42 +1,61 @@
-MAX_CUBES_IN_BAG = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
+def get_game_number(game_str: str) -> int:
+    return int(game_str.split()[-1])
 
 
-def get_cubes(line: str):  # TODO: Rename
-    game_str, cubes_str = line.split(': ')
-
-    game_number = int(game_str.split()[-1])
-
-    draws = cubes_str.split('; ')
-
-    CUBES_WE_KNOW = {
+def get_known_cubes_amount(draws_str: str) -> dict[str, int]:
+    cubes_in_bag = {
         'red': 0,
         'green': 0,
         'blue': 0
     }
+    draws = draws_str.split('; ')
 
     for draw in draws:
-        colors = draw.split(', ')
-        for color in colors:
-            count, key = color.split()
-            if CUBES_WE_KNOW[key] < int(count):
-                CUBES_WE_KNOW[key] = int(count)
+        cubes = draw.split(', ')
 
-    for key, value in MAX_CUBES_IN_BAG.items():
-        if CUBES_WE_KNOW[key] > value:
-            return 0
+        for single_color_cubes in cubes:
+            amount_str, color = single_color_cubes.split()
 
-    return game_number
+            amount = int(amount_str)
+
+            if cubes_in_bag[color] < amount:
+                cubes_in_bag[color] = amount
+
+    return cubes_in_bag
+
+
+def is_game_possible(known_cubes_in_bag: dict[str, int]) -> bool:
+    max_cubes_in_bag = {
+        'red': 12,
+        'green': 13,
+        'blue': 14
+    }
+
+    for color, max_amount in max_cubes_in_bag.items():
+        if known_cubes_in_bag[color] > max_amount:
+            return False
+
+    return True
+
+
+def get_single_game_result(line: str) -> int:
+    game_str, draws_str = line.split(': ')
+
+    game_number = get_game_number(game_str)
+
+    known_cubes_amount = get_known_cubes_amount(draws_str)
+
+    if is_game_possible(known_cubes_amount):
+        return game_number
+
+    return 0
 
 
 def solution(text_lines: list[str]) -> int:
     result = 0
 
     for line in text_lines:
-        result += get_cubes(line)
+        result += get_single_game_result(line)
 
     return result
 
